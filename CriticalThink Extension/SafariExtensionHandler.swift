e86e1eff71edf70b63f0f6b8a02449cb0d7b1f82
Brandon Main <brandonlmain@gmail.com>
2020-09-10 12:59:48 -0700

    initial

--- CriticalThink Extension/SafariExtensionHandler.swift
+++ CriticalThink Extension/SafariExtensionHandler.swift
@@ -12,16 +12,11 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     
     override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
         // This method will be called when a content script provided by your extension calls safari.extension.dispatchMessage("message").
-        page.getPropertiesWithCompletionHandler { properties in
-            NSLog("The extension received a message (\(messageName)) from a script injected into (\(String(describing: properties?.url))) with userInfo (\(userInfo ?? [:]))")
+        if messageName == "ANALYZE" {
+            analyzeMessage(message: userInfo)
         }
     }
-    
-    override func toolbarItemClicked(in window: SFSafariWindow) {
-        // This method will be called when your toolbar item is clicked.
-        NSLog("The extension's toolbar item was clicked")
-    }
-    
+
     override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
         // This is called when Safari's state changed in some way that would require the extension's toolbar item to be validated again.
         validationHandler(true, "")
@@ -30,5 +25,17 @@ class SafariExtensionHandler: SFSafariExtensionHandler {
     override func popoverViewController() -> SFSafariExtensionViewController {
         return SafariExtensionViewController.shared
     }
-
+    
+    
+    override func popoverWillShow(in window: SFSafariWindow) {
+        window.getActiveTab { (tab) in
+            tab?.getActivePage(completionHandler: { (page) in
+                page?.dispatchMessageToScript(withName: "GET", userInfo: nil)
+            })
+        }
+    }
+    
+    private func analyzeMessage(message: [String : Any]?) {
+        NSLog(message?["text"] as! String)
+    }
 }
--- CriticalThink Extension/script.js
+++ CriticalThink Extension/script.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    safari.extension.dispatchMessage("Hello World!");
-});
+safari.self.addEventListener("message", handleMessage);
+
+
+function handleMessage(event) {
+    if(event.name === "GET") {
+        getPageContent();
+    }
+}
+
+function getPageContent() {
+    let body = document.body.textContent;
+    safari.extension.dispatchMessage("ANALYZE", { "text": body });
+}
